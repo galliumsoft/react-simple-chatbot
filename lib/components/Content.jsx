6f9aa7a15d37c5223263b1305572a69c98a83073
Madhukar I B <galliumsoft@gmail.com>
2019-03-25 22:47:35 +0000

    simple cosmetic changes

--- lib/components/Content.jsx
+++ lib/components/Content.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Content = styled.div`
   height: calc(${props => props.height} - ${props => props.hideInput ? '56px' : '112px'});
-  overflow-y: scroll;
+  overflow-y: auto;
   margin-top: 2px;
   padding-top: 6px;
 
